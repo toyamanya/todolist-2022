5dbb92b117e78611df4ebc5e5fbc8d3fc8c18d04
toyamanya <ryotoyamaiii@gmail.com>
2022-05-18 23:28:31 +0900

    高額商品とか

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup>
 import ClickCounter from "./ClickCounter.vue";
+import ItemList from "./ItemList.vue";
 
 defineProps({
   msg: { type: String, default: "" },
@@ -10,6 +11,7 @@ defineProps({
   <div>
     <h1>{{ msg }}</h1>
     <ClickCounter />
+    <ItemList />
   </div>
 </template>
 
